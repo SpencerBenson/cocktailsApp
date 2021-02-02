2cc88eddde18324417b3e7003388dcc7ae1c8e47
Benson Njuguna K <bennspencer@gmail.com>
2021-02-02 17:07:59 +0300

    removed sidebar. Unnecessary extra

---
+++ src/components/SideBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+// import {
+//   Collapse,
+//   Navbar,
+//   NavbarToggler,
+//   NavbarBrand,
+//   Nav,
+//   NavItem,
+//   NavLink,
+//   UncontrolledDropdown,
+//   DropdownToggle,
+//   DropdownMenu,
+//   DropdownItem,
+//   NavbarText,
+//   Form,
+//   Input,
+//   Button
+// } from 'reactstrap';
+// import { FaSearch } from 'react-icons/fa';
+
+const SideBar = (props) => {
+  // const [isOpen, setIsOpen] = useState(false);
+
+  // const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div className='sidebar'>
+    Sidebar
+    </div>
+  );
+}
+
+export default SideBar;
