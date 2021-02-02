df9320d123a38341c4399395b5a18857603932db
Benson Njuguna K <bennspencer@gmail.com>
2021-02-02 16:25:42 +0300

    cocktail category cards added

---
+++ src/components/CocktailCategoryCard.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button,
+  CardLink,Col
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+const CocktailCategoryCard = (props) => {
+  const drinkDetails = {...props.drink}
+  const path = `/cocktail/${drinkDetails.idDrink}`
+  return (
+    <Col xs='4' key={drinkDetails.idDrink}>
+      <Card className="cocktail-card" >
+        <CardImg top width="100%" src={drinkDetails.strDrinkThumb} alt="Card image cap" />
+        <CardBody>
+  <CardTitle tag="h5">{drinkDetails.strDrink}</CardTitle>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">{drinkDetails.strAlcoholic}</CardSubtitle>
+          {/* <CardText>{drinkDetails.strAlcoholic} {intro}.</CardText> */}
+          <Link to={path}>
+          <Button className='btn btn-success'>View</Button>
+          </Link>
+        </CardBody>
+      </Card>
+      </Col>
+  );
+};
+
+export default CocktailCategoryCard;
+Card.propTypes = {
+  // Pass in a Component to override default element
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  inverse: PropTypes.bool,
+  color: PropTypes.string,
+  body: PropTypes.bool,
+  className: PropTypes.string
+};
+
+
+
+CardImg.propTypes = {
+  // Pass in a Component to override default element
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  className: PropTypes.string,
+  // Use top or bottom to position image via "card-img-top" or "card-img-bottom"
+  top: PropTypes.bool,
+  bottom: PropTypes.bool
+};
+
+
+CardLink.propTypes = {
+  // Pass in a Component to override default element
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  className: PropTypes.string,
+  // ref will only get you a reference to the CardLink component, use innerRef to get a reference to the DOM element (for things like focus management).
+  innerRef: PropTypes.oneOfType([PropTypes.func, PropTypes.string])
+};
+
+CardSubtitle.propTypes = {
+  // Pass in a Component to override default element
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  className: PropTypes.string
+};
+
+CardText.propTypes = {
+  // Pass in a Component to override default element
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  className: PropTypes.string
+};
+
+CardTitle.propTypes = {
+  // Pass in a Component to override default element
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+  className: PropTypes.string
+};
